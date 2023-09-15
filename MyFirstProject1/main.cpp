bc93d329548ce8d6b34ed9ec6e3456565475bf0d
Ty-S <89536187+Ty-Schluter@users.noreply.github.com>
2023-09-14 19:03:41 -0700

    Add project files.

---
+++ MyFirstProject1/main.cpp
@@ -0,0 +1,22 @@
+/*
+Ty Schluter
+C++ Semester 1 2023
+Due: 9 / 15
+Lab 1 Introduction to C++ Programming
+• Description : This lab will serve as a demonstration of your ability to download a project from
+GitHub, compile and run the project, upload a project to GitHub, and submit a project to Canvas.
+This lab assumes you have downloaded and installed an IDE(integrated development
+	environment) and have also installed GitHub Desktop.If you have not met the assumptions for
+	the lab, the steps you need to complete the lab may vary from what is here.The goals are for
+	you to download a project, compile and run that project, and to upload your own project.If you
+	meet the goals, you will get the points for this lab.
+*/
+
+#include <iostream>
+int main()
+{
+	std::cout << "Hello World!" << std::endl; // Outputs Hello World because of the "cout"
+	std::cout << "My favorite color is red!" << std::endl; // Same of comment above
+	return 0;
+}
+
